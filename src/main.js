ab357dfa6ce86876e60c0aba113d1bf7c4c8ac65
Helder Traci <helderdiin.work@gmail.com>
2017-06-20 20:28:55 -0300

    Importing i18n configuration and initializing on app

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'element-ui/lib/theme-default/index.css';
 
 import App from './App';
 import router from './router';
+import i18n from './i18n';
 
 Vue.use(ElementUI);
 
@@ -14,5 +15,6 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
+  i18n,
   render: h => h(App),
 });
