0cf8de9f01d6d0dcda6ed3e18695451cb83b3ed6
Helder Traci <helderdiin.work@gmail.com>
2018-08-18 11:13:43 -0300

    Adding compression plugin for webpack

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    ],
+  },
+};
