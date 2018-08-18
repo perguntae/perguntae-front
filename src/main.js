1cff088aca68989bb96924e1fe4f384a60b5867a
Helder Traci <helderdiin.work@gmail.com>
2018-08-18 11:43:05 -0300

    Adding i18n features

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+
 import App from './App.vue';
+
 import router from './router';
 import store from './store';
+import messages from './i18n';
+
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: 'pt-br',
+  messages,
+});
+
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App),
 }).$mount('#app');
