927b686e0e70174288364b9ea158765788abd1aa
Helder Traci <helderdiin.work@gmail.com>
2018-08-19 13:03:21 -0300

    Adding webpack chunk for room page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import Room from '@/views/Room';
-
 Vue.use(Router);
 
 export default new Router({
@@ -12,7 +10,7 @@ export default new Router({
     {
       path: '/room',
       name: 'room',
-      component: Room,
+      component: () => import(/* webpackChunkName: "room" */ '@/views/Room'),
       children: [
         {
           path: 'create',
