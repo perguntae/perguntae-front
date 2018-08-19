10fb4250efce0f5341c5bb88dbef0179aad4361d
Helder Traci <helderdiin.work@gmail.com>
2018-08-19 12:46:25 -0300

    Adding pug and basic style file

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+
+  #app
+    el-container
+      el-header Header
+      el-main
+        router-view
+      el-footer Footer
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped lang="scss">
+@import './style/style.scss';
+
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
   text-align: center;
-  color: #2c3e50;
+  line-height: 60px;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
 }
 </style>
