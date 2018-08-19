fca14c4f75890a3eb33d6e3a31d26537bc705148
Helder Traci <helderdiin.work@gmail.com>
2018-08-19 12:53:24 -0300

    Creating list page

---
+++ src/views/Room/list.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+
+  #list-container
+    el-table(:data="rooms" style="width: 100%")
+      el-table-column(prop="name" label="Name" width="180")
+      el-table-column(prop="description" label="Description" width="180")
+      el-table-column(prop="hash" label="Hash")
+
+</template>
+
+<script>
+import RoomMixin from '../../mixin/room';
+
+export default {
+  name: 'ListRoom',
+  mixins: [RoomMixin],
+  created() {
+    this.listRoom();
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
