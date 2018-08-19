23ad02a2755fe674af6ee46997016a60fc7c1a50
Helder Traci <helderdiin.work@gmail.com>
2018-08-19 12:53:40 -0300

    Creating create page

---
+++ src/views/Room/create.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+
+  #create-container
+    el-form(label-position="right" label-width="100px" :model="formRoom")
+      el-form-item(:label="$t('room.create.label.title')")
+        el-input(v-model="formRoom.title")
+      el-form-item(:label="$t('room.create.label.description')")
+        el-input(v-model="formRoom.description")
+      el-form-item
+        el-button(type="primary" @click="createClass") {{ $t('room.create.button.create') }}
+
+</template>
+
+<script>
+import RoomMixin from '../../mixin/room';
+
+export default {
+  name: 'CreateRoom',
+  mixins: [RoomMixin],
+  methods: {
+    createClass() {
+      this.createRoom();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
